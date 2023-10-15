5f92b3407e6351b723162cb0fd1e9f0b7b46d812
Peter Ramm <Peter@ramm-oberhermsdorf.de>
2023-10-15 13:57:03 +0200

    Generate menu content as html

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 # Build and deploy artifacts
 
-echo "AsciiDoc -> html"
-./generate_adoc_html.sh
 
 echo "Generate panorama_content.html"
 ./generate_content_html.sh
--- generate_adoc_html.sh
+++
@@ -1,2 +0,0 @@
-echo "AsciiDoc -> html"
-asciidoctor -o Oracle_performance_analysis_with_Panorama_from_adoc.html Oracle_performance_analysis_with_Panorama.adoc
